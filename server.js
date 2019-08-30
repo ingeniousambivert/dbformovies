1775077993993347fa46641b0634dfc83573507a
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2019-08-30 16:01:54 +0530

    Updated Server

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + "/dist/dbformovies/"));
+app.use(express.static(__dirname + "/dbformovies/dist/"));
 
 // Send all requests to index.html
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/dbformovies/index.html"));
+  res.sendFile(path.join(__dirname + "/dbformovies/dist/index.html"));
 });
 
 // default Heroku PORT
