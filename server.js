abeda5d0c5102ad8bb74d5352c5f6848bc39e3ff
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2019-08-30 16:16:04 +0530

    Updated gitignore

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + "/dbformovies/dist/"));
+app.use(express.static(__dirname + "/dist/dbformovies"));
 
 // Send all requests to index.html
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dbformovies/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/dbformovies/index.html"));
 });
 
 // default Heroku PORT
