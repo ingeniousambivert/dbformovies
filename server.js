1166e446092e6f69b12c215070faaee3ad1cd47e
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2019-08-30 16:48:10 +0530

    Updated scripts for Heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + "./dist/dbformovies"));
+app.use(express.static(__dirname, "./dist/dbformovies"));
 
 // Send all requests to index.html
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "dist/dbformovies/index.html"));
+  res.sendFile(path.join(__dirname, "dist/dbformovies/index.html"));
 });
 
 // process.env.PWD = process.cwd();
