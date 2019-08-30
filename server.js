1e373063fd5aa7a1f8eb345a69c47b120f5c6742
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2019-08-30 16:39:30 +0530

    Updated server for Heroku

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + "/dist/dbformovies"));
+// // Serve static files
+// app.use(express.static(__dirname + "/dist/dbformovies"));
 
-// Send all requests to index.html
+// // Send all requests to index.html
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/dist/dbformovies/index.html"));
+// });
+
+process.env.PWD = process.cwd();
+
+app.use(express.static(process.env.PWD + "/build"));
+
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/dbformovies/index.html"));
+  const index = path.join(process.env.PWD, "/build/index.html");
+  res.sendFile(index);
 });
 
 // default Heroku PORT
