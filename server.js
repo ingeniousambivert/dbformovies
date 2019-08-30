fe583610edd822a23ea41ee580bbe70916cf0407
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2019-08-30 16:00:51 +0530

    Updated Server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + "/dist/dbformovies"));
+app.use(express.static(__dirname + "/dist/dbformovies/"));
 
 // Send all requests to index.html
 app.get("/*", (req, res) => {
